f7b39255ca4613ea5bca4cf7c1bf2f5774bd90ae
Ryan Sonnek <ryan@socialcast.com>
2009-10-03 18:33:03 -0500

    remove default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => :flash, :action => :notice
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
